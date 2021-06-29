aad8a665a1587068b9bcbc26feb4772d1948e980
Darya <darya23m@gmail.com>
2021-06-29 20:26:14 +0300

    [AD-8] Navigation bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,10 +8,33 @@ const Header = () => {
        <div></div>
        <div>
         <div className={styles.nav}>
-          Menu
+          <ul>
+            <li className={styles.nav_list_item}>
+              <a href='#ADISA'>ADISA</a>
+            </li>
+            <li className={styles.nav_list_item}>
+              <a href='#Equipment'>Оборудование</a>
+            </li>
+            <li className={styles.nav_list_item}>
+              <a href='#Projectors'>Проектировщику</a>
+            </li>
+            <li className={styles.nav_list_item}>
+              <a href='#Offices'>Представительства</a>
+            </li>
+            <li className={styles.nav_list_item}>
+              <a href='#Contacts'>Контакты</a>
+            </li>
+          </ul>
         </div>
         <div className={styles.lang}>
-          Ru Ua
+          <ul className={styles.lang_list}>
+            <li  className={styles.lang_item}>
+              <button className={styles.lang_button}>RU</button>
+            </li>
+            <li>
+              <button className={styles.lang_button}>UA</button>
+            </li>
+          </ul>
         </div>
        </div>
        <div></div>
