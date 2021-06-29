ce093ce41c0583d0b5f3c15e1fb094f7fb70e693
Darya <darya23m@gmail.com>
2021-06-29 10:02:04 +0300

    initial structure

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
