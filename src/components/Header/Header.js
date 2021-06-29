e313dd46cf5a0c2cf44c1ccb1cf0da8bc002ef7f
Darya <darya23m@gmail.com>
2021-06-30 01:47:54 +0300

    [AD-9] Language toggle

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,32 +9,26 @@ const Header = () => {
        <div>
         <div className={styles.nav}>
           <ul>
-            <li className={styles.nav_list_item}>
+            <li>
               <a href='#ADISA'>ADISA</a>
             </li>
-            <li className={styles.nav_list_item}>
+            <li>
               <a href='#Equipment'>Оборудование</a>
             </li>
-            <li className={styles.nav_list_item}>
+            <li>
               <a href='#Projectors'>Проектировщику</a>
             </li>
-            <li className={styles.nav_list_item}>
+            <li>
               <a href='#Offices'>Представительства</a>
             </li>
-            <li className={styles.nav_list_item}>
+            <li>
               <a href='#Contacts'>Контакты</a>
             </li>
           </ul>
         </div>
         <div className={styles.lang}>
-          <ul className={styles.lang_list}>
-            <li  className={styles.lang_item}>
-              <button className={styles.lang_button}>RU</button>
-            </li>
-            <li>
-              <button className={styles.lang_button}>UA</button>
-            </li>
-          </ul>
+          <a href='#RU' className={styles.active}>Рус</a>
+          <a href='#UA'>Укр</a>
         </div>
        </div>
        <div></div>
