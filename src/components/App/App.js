f800596142969c2e74e2eccc011c8ddc31abbbfd
Darya <darya23m@gmail.com>
2021-06-30 21:41:38 +0300

    [AD-11] Intro Section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from 'components/Header/Header';
+import Intro from 'components/Intro/Intro';
 
 //import styles from './App.module.scss';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <React.Fragment>
       <Header />
+      <Intro />
     </React.Fragment>
   );
 }
