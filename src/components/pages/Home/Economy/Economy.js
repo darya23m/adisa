023cd626b549606df0aeea1b84834e69c845e2ab
Darya <darya23m@gmail.com>
2021-07-14 10:58:21 +0300

    refactoring

--- src/components/pages/Home/Economy/Economy.js
+++ src/components/pages/Home/Economy/Economy.js
@@ -2,50 +2,36 @@ import React from 'react';
 
 import styles from './Economy.module.scss';
 
-import divider from './divider.svg'
-
-const Economy = () => {
+const Economy = ({ data }) => {
   return (
-    <div className={styles.economy}>
-      <h1>Котлы ADISA - это</h1>
-      <div className={styles.divider}>
-        <img src={divider} alt='divider' />
-      </div>
-      <div className={styles.title}>
-        <span>1</span>
-        <h2> Минимальный расход газа</h2>
-      </div>
-      <h3>
-      Благодаря высокой эффективности конденсационных котлов<br />
-      <b>расход газа ниже на 15%, чем у обычных жаротрубных котлов. </b>
-      </h3>
-      <div className={styles.calculator}>
-        <div className={styles.calculator_power}>
-          <div className={styles.calculator_power_descr}>
-            <label>
-            Введите мощность котельной,<br /> 
-            чтобы увидеть свою экономию
-            </label>
-          </div>
-          <input type='number'></input>
-          <p>кВт</p>
-          <button>Посчитать</button>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.title}>
+          <span>{data.title.number}</span>
+          <h2>{data.title.text}</h2>
         </div>
-        <div className={styles.calculator_title}>
-          Ваша <span>экономия газа</span> в сравнении с обычным котлом составит:
-        </div>
-        <div className={styles.graf}>
-          <div className={styles.graf_grid}>
-            <div className={styles.graf_grid_first}></div>
-            <div className={styles.graf_grid_second}></div>
-            <div className={styles.graf_grid_third}></div>
-            <div className={styles.graf_grid_forth}></div>
+        <h3 className={styles.subtitle} dangerouslySetInnerHTML={{__html: data.subtitle}}></h3>
+        <div className={styles.calculator}>
+          <div className={styles.power}>
+            <label className={styles.powerDescr}>{data.label}</label>
+            <input type='number' className={styles.powerInput}></input>
+            <p className={styles.unit}>{data.unit}</p>
+            <button className={styles.calculate}>Посчитать</button>
           </div>
-          <div className={styles.graf_years}>
-            <div className={styles.graf_years_one}>За год</div>
-            <div className={styles.graf_years_five}>За 5 лет</div>
-            <div className={styles.graf_years_ten}>За 10 лет</div>
-            <div className={styles.graf_years_twenty}>За 20 лет</div>
+          <div className={styles.grafTitle} dangerouslySetInnerHTML={{__html: data.description}}></div>
+          <div className={styles.graf}>
+            <div className={styles.grafGrid}>
+              <div className={styles.gridFirst}></div>
+              <div className={styles.gridSecond}></div>
+              <div className={styles.gridThird}></div>
+              <div className={styles.gridForth}></div>
+            </div>
+            <div className={styles.grafYears}>
+              <div className={styles.yearsOne}>{data.grafYears[0]}</div>
+              <div className={styles.yearsFive}>{data.grafYears[1]}</div>
+              <div className={styles.yearsTen}>{data.grafYears[2]}</div>
+              <div className={styles.yearsTwenty}>{data.grafYears[3]}</div>
+            </div>
           </div>
         </div>
       </div>
--- src/content/ru/index.js
+++ src/content/ru/index.js
@@ -1,5 +1,6 @@
 import header from './header';
 import footer from './footer';
+import home from './home';
 import equipment from './equipment';
 import info from './info';
 import offices from './offices';
@@ -8,6 +9,7 @@ import contacts from './contacts';
 export default {
   header,
   footer,
+  home,
   equipment,
   info,
   offices,
