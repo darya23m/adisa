abe3a9f4cc66f21af6bb80bc7ee0966084e6cd2b
Darya <darya23m@gmail.com>
2021-07-16 19:48:08 +0300

    fixed markup Repairs section

---
+++ src/components/pages/Home/Repairs/Repairs.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ReactSVG } from 'react-svg';
+
+import styles from './Repairs.module.scss';
+import service from './Service.svg';
+
+const Repairs = ({ data }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <div className={styles.titleNumber}>{data.title.number}</div>
+        <h2 className={styles.titleText}>{data.title.text}</h2>
+      </div>
+      <div className={styles.content}>
+        <ReactSVG src={service} alt='SERVICES' width='428' height='372' />
+        <p className={styles.repairBlockText}>{data.repairBlockText}
+        <a href='#Services' className={styles.services}>{data.services}</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Repairs;
