47a632dd3282af9aebb40b9b9dd5a175d45e1efd
Darya <darya23m@gmail.com>
2021-07-16 12:06:25 +0300

    fixed markup Quality section

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -5,6 +5,7 @@ import MainTitle from './MainTitle/MainTitle';
 import Economy from './Economy/Economy';
 import Power from './Power/Power';
 import NoiseLvl from './NoiseLvl/NoiseLvl';
+import Quality from './Quality/Quality';
 
 function Home({ data }) {
   return (
@@ -14,6 +15,7 @@ function Home({ data }) {
       <Economy data={data.economy} />
       <Power data={data.power}/>
       <NoiseLvl data={data.noiseLvl} />
+      <Quality data={data.quality} />
     </>
   )
 }
