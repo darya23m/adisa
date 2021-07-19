e0fd0899940ac094446281f44400ec7707abc325
Darya <darya23m@gmail.com>
2021-07-19 16:42:35 +0300

    fixed Training section

--- src/components/pages/Home/Training/Training.js
+++ src/components/pages/Home/Training/Training.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
- import styles from './Training.module.scss';
+import Title from 'components/common/Title/Title';
+import styles from './Training.module.scss';
 
- const Training = ({ data }) => {
-   return (
-     <div className={styles.container}>
-      <div className={styles.title}>
-        <div className={styles.titleNumber}>{data.title.number}</div>
-        <h2 className={styles.titleText}>{data.title.text}</h2>
-      </div>
+const Training = ({ data }) => {
+  return (
+    <div className={styles.container}>
+      <Title title={data.title} number="6" isWhite />
       <div className={styles.content}>
         <div className={styles.trainingText}>{data.trainingText}</div>
         <button className={styles.buttonVideo}>{data.buttonText}</button>
       </div>
-     </div>
-   );
- };
+    </div>
+  );
+};
 
- export default Training;
+export default Training;
  
