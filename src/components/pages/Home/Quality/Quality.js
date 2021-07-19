78909e9d00427a681d60a7b88fa3c9842d65faa5
Darya <darya23m@gmail.com>
2021-07-19 15:20:31 +0300

    fixed Quality section

--- src/components/pages/Home/Quality/Quality.js
+++ src/components/pages/Home/Quality/Quality.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
- import styles from './Quality.module.scss';
- import siemens from './Siemens.jpg';
+import Title from 'components/common/Title/Title';
+import styles from './Quality.module.scss';
+import siemens from './img/Siemens.jpg';
+import parseStrWithBoldElems from 'utils/parseStrWithBoldElems';
 
- const Quality = ({ data }) => {
-   return (
-     <div className={styles.container}>
-      <div className={styles.title}>
-        <div className={styles.titleNumber}>{data.title.number}</div>
-        <h2 className={styles.titleText}>{data.title.text}</h2>
-      </div>
-      <div className={styles.content}>
-        <img src={siemens} alt='Siemens' width='408' height='269' className={styles.siemens} />
-        <div className={styles.qualityText}>
-          <p className={styles.qualityTextP1} dangerouslySetInnerHTML={{__html: data.qualityTextP1}}></p>
-          <p className={styles.qualityTextP2} dangerouslySetInnerHTML={{__html: data.qualityTextP2}}></p>
-          <p className={styles.qualityTextP3} dangerouslySetInnerHTML={{__html: data.qualityTextP3}}></p>
+const Quality = ({ data }) => {
+  return (
+    <div className={styles.container}>
+      <Title title={data.title} number="4" isWhite />
+      <div className={styles.content}> 
+        <div className={styles.description}>
+          <div className={styles.qualityText}>
+            <p className={styles.qualityTextP1}>{ parseStrWithBoldElems(data.qualityTextP1) }</p>
+            <p className={styles.qualityTextP2}>{ parseStrWithBoldElems(data.qualityTextP2) }</p>
+            <p className={styles.qualityTextP3}>{ parseStrWithBoldElems(data.qualityTextP3) }</p>
+          </div>
+          <div className={styles.siemensImg}><img src={siemens} alt={data.siemensAlt} className={styles.siemens} /></div>
         </div>
       </div>
-     </div>
-   );
- };
+    </div>
+  );
+};
 
- export default Quality;
+export default Quality;
