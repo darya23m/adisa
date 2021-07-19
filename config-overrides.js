793f789c976ce7dec6e6e67c85e140da23f93ef4
Viktor Klymentiev <viclime@gmail.com>
2021-07-20 00:26:51 +0300

    try to disable prerender-spa-plugin

--- config-overrides.js
+++ config-overrides.js
@@ -21,18 +21,18 @@ const ROUTES = [
 //   ROUTES.push()
 // });
 
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-const path = require('path');
+// const PrerenderSPAPlugin = require('prerender-spa-plugin');
+// const path = require('path');
 
 module.exports = (config, env) => {
-  if (env === 'production') {
-    config.plugins = config.plugins.concat([
-      new PrerenderSPAPlugin({
-        routes: ROUTES,
-        staticDir: path.join(__dirname, 'build'),
-      }),
-    ]);
-  }
+  // if (env === 'production') {
+  //   config.plugins = config.plugins.concat([
+  //     new PrerenderSPAPlugin({
+  //       routes: ROUTES,
+  //       staticDir: path.join(__dirname, 'build'),
+  //     }),
+  //   ]);
+  // }
 
   return config;
 };
