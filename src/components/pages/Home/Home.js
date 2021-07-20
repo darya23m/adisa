0904dd3d708f75c4600e24bd3e5a08c4af0c597a
Darya <darya23m@gmail.com>
2021-07-20 12:23:04 +0300

    footer-viewport

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -15,13 +15,13 @@ function Home({ data }) {
     <>
       <Intro data={data.intro} />
       <Tagline data={data.tagline} />
-      <Economy data={data.economy} />
+      {/* <Economy data={data.economy} />
       <Power data={data.power}/>
       <NoiseLvl data={data.noiseLvl} />
       <Quality data={data.quality} />
       <Pollution data={data.pollution} />
       <Training data={data.training} />
-      <Repairs data={data.repairs} />
+      <Repairs data={data.repairs} /> */}
     </>
   )
 }
