3623b2fdee09b60455a3d3a3ccbb47bcca5e5c0d
darya23m <77686522+darya23m@users.noreply.github.com>
2021-07-27 18:18:28 +0300

    Offices page fix (#29)

--- src/components/pages/Offices/Office.js
+++ src/components/pages/Offices/Office.js
@@ -1,55 +1,42 @@
 import React, { useState } from 'react';
-import cx from 'classnames';
+import { Link } from 'react-router-dom';
 
 import styles from './Office.module.scss';
-import {ReactComponent as ArrowIconClose} from './img/Down.svg';
-import {ReactComponent as ArrowIconOpen} from './img/Up.svg';
+import {ReactComponent as ArrowIcon} from './img/Up.svg';
 
-const Office = ({ data: {city, addresses} }) => {
-  const [isOpened, setIsOpened ] = useState(false);
-
+const Office = ({ data: {city, addresses}, parentUrl }) => {
   const renderContactsItem = (list, label, type) => 
-  list.map((curr, index) => (
-    <div key={index} className={styles.contactsItem}>
-      { label }:
-      {' '}
-      { type === 'tel' && <a href={`tel:${curr}`} className={styles.contact}>{curr}</a> }
-      { type === 'email' && <a href={`mailto:${curr}`} className={styles.contact}>{ curr }</a> }
-      { type === 'web' && <a href={curr} className={styles.contact}>{ curr }</a> }
-    </div>
+    list.map((curr, index) => (
+      <div key={index} className={styles.contactsItem}>
+        { label }:
+        {' '}
+        { type === 'tel' && <a href={`tel:${curr}`} className={styles.contact}>{curr}</a> }
+        { type === 'email' && <a href={`mailto:${curr}`} className={styles.contact}>{ curr }</a> }
+        { type === 'web' && <a href={curr} className={styles.contact}>{ curr }</a> }
+      </div>
+      ));
+
+  const renderAddresses = () => addresses.map((curr, index) => (
+      <li key={index} className={styles.listItem}>
+        <div className={styles.listItemBlock}>
+          <div key={index} className={styles.address}>{ curr.address }</div>
+          { curr.tels && renderContactsItem(curr.tels, 'Тел.', 'tel') }
+          { curr.emails && renderContactsItem(curr.emails, 'E-mail', 'email') }
+          { curr.webs && renderContactsItem(curr.webs, 'Web', 'web') }
+        </div>
+      </li>
     ));
 
-  const renderAddresses = () => {
-    return (
-      <div className={styles.listAddresses}>
-        <ul className={styles.list}>
-          { addresses.map((curr, index) =>
-            <li key={index} className={styles.listItem}>
-              <div className={styles.listItemBlock}>
-                <div key={index} className={styles.address}>{ curr.address }</div>
-                { curr.tels && renderContactsItem(curr.tels, 'Тел.', 'tel') }
-                { curr.emails && renderContactsItem(curr.emails, 'E-mail', 'email') }
-                { curr.webs && renderContactsItem(curr.webs, 'Web', 'web') }
-              </div>
-            </li>
-          )
-        }
-        </ul>
-      </div>
-    );
-  }
-
   return (
-    <>
-      <div className={!isOpened ? styles.caption : styles.captionOpened}>
-        {!isOpened ? 
-        <ArrowIconClose className={styles.arrowIcon} onClick={() => setIsOpened(true)}/> 
-        : 
-        <ArrowIconOpen className={styles.arrowIcon} onClick={() => setIsOpened(false)}/>}
+    <div className={styles.element}>
+      <Link className={styles.caption} to={parentUrl}>
+        <ArrowIcon className={styles.arrowIcon}/>
         <div className={styles.city}>{ city }</div>
-      </div>
-      {isOpened && renderAddresses()}
-    </>
+      </Link>
+      <ul className={styles.list}>
+        { renderAddresses() }
+      </ul>
+    </div>
   );
 };
 
