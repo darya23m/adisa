ec3cdd38f981de5614b485e2359dcc28738e3db1
Darya <darya23m@gmail.com>
2021-07-27 18:16:50 +0300

    used router for offices list

--- src/components/pages/Offices/Offices.js
+++ src/components/pages/Offices/Offices.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import { Switch, Route, Link, useRouteMatch, matchPath, useLocation } from 'react-router-dom';
 
 import styles from './Offices.module.scss';
 import Office from './Office';
+import {ReactComponent as ArrowIcon} from './img/Down.svg';
 
-const Offices = ({ data }) => {
-  const renderLists = (offices) => offices.map((office, index) => (<Office key={index} data={office} />));
+const Offices = ({ data: { title, offices } }) => {
+  const { pathname } = useLocation();
+  const { path, url } = useRouteMatch(); 
+
+  const { params: { current } } = matchPath(pathname, { path: `${path}/:current` }) || {params: {}};
+  const currIndex = offices.map((o) => o.path).indexOf(current);
+  const officesBefore = offices.slice(0, currIndex);
+  const officesAfter = current ? offices.slice(currIndex+1) : [];
+
+  const officeLinks = (list) => list.map((item) => (
+    <Link to={`${url}/${item.path}`} className={styles.item}>
+      <ArrowIcon className={styles.arrowIcon}/>
+      <div className={styles.city}>{ item.city }</div>
+    </Link>
+  ));
+
+  const officeRoutes = (list) => list.map((item) => (
+    <Route path={`${path}/${item.path}`}>
+      <Office data={item} parentUrl={url} />
+    </Route>
+  ))
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>{data.title}</div>
-        <div className={styles.content}>
-          { renderLists(data.offices) }
-        </div>
+      <div className={styles.header}>{title}</div>
+      <div className={styles.content}>
+        {officeLinks(officesBefore)}
+        <Switch>
+          {officeRoutes(offices)}
+        </Switch>
+        {officeLinks(officesAfter)}
+      </div>
     </div>
   );
 };
