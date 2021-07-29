5e1ea9e1f5bf77c426b7944b51953ec4afcb5c2d
darya23m <77686522+darya23m@users.noreply.github.com>
2021-07-29 19:36:29 +0300

    Routing improvements (#32)

--- src/content/ru/header.js
+++ src/content/ru/header.js
@@ -1,7 +1,7 @@
 export default {
   links: {
     home: 'ADISA',
-    equipment: 'Оборудование',
+    "equipment/adi_lt": 'Оборудование',
     info: 'Проектировщику',
     offices: 'Представительства',
     contacts: 'Контакты'
