7cc600d3972a84453ec7d6b062da00d7e399d686
Darya <darya23m@gmail.com>
2021-08-05 20:11:31 +0300

    [AD-28] fix 2

--- src/components/pages/Home/NoiseLvl/NoiseLvl.js
+++ src/components/pages/Home/NoiseLvl/NoiseLvl.js
@@ -39,15 +39,15 @@ const NoiseLvl = ({ data: {
             <div className={styles.redLine} />
             <div className={styles.feature}>
               <div className={styles.name1}>{ featureName1 }</div>
-              <div><img src={feature1} alt={featureAlt1} className={styles.imageFeature} /></div>              
+              <img src={feature1} alt={featureAlt1} className={styles.imageFeature} />
             </div>
             <div className={styles.feature}>
               <div className={styles.name2}>{ featureName2 }</div>
-              <div><img src={feature2} alt={featureAlt2} className={styles.imageFeature} /></div>
+              <img src={feature2} alt={featureAlt2} className={styles.imageFeature} />
             </div>
             <div className={styles.feature}>
               <div className={styles.name3}>{ featureName3 }</div>
-              <div><img src={feature3} alt={featureAlt3} className={styles.imageFeature} /></div>
+              <img src={feature3} alt={featureAlt3} className={styles.imageFeature} />
             </div>
           </div>
         </div>
