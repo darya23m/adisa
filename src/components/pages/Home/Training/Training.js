706229da89c36740ed2e0346eaa9dd550828fd5f
darya23m <77686522+darya23m@users.noreply.github.com>
2021-08-17 18:47:16 +0300

    Revert "Debugging (#81)"
    This reverts commit 3e1bfc31b22facf0b90919eac4d0e23a09df2d21.

--- src/components/pages/Home/Training/Training.js
+++ src/components/pages/Home/Training/Training.js
@@ -8,14 +8,17 @@ import styles from './Training.module.scss';
 const Training = ({ data: { title, trainingText, watchButtonText }}) => {
   return (
     <div className={styles.container}>
-      <video width="100%" height="100%" autoPlay controls={false} loop muted={true}>
+      <video width="100%" autoPlay controls={false} loop muted={true}>
         <source src={factoryPath} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className={styles.content}>
-        <Title title={title} number="6" isWhite />
-        <p className={styles.trainingText}>{ parseStrWithBoldElems(trainingText) }</p>
+        <div>
+          <Title title={title} number="6" isWhite />
+          <p className={styles.trainingText}>{ parseStrWithBoldElems(trainingText) }</p>
+        </div>
         <button type="button" className={styles.watchBtn}>{ watchButtonText }</button>
+        <div />
       </div>
     </div>
   );
