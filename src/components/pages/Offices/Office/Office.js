83167db821d91fee18c353e0e0e5c22f176eb308
Евгений Климентьев <jmnemo@MacBook-Pro-Evgenij.local>
2021-08-18 23:32:57 +0300

    refactoring offices page

--- src/components/pages/Offices/Office/Office.js
+++ src/components/pages/Offices/Office/Office.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import styles from './Office.module.scss';
-import {ReactComponent as ArrowIcon} from './img/Up.svg';
+import { ReactComponent as ArrowDownIcon } from './img/Down.svg';
+import { ReactComponent as ArrowUpIcon } from './img/Up.svg';
 
-const Office = ({ data: {city, addresses}, parentUrl }) => {
+const Office = ({ data: { city, addresses, path }, parentUrl }) => {
+  const location = useLocation();
+  const currOfficePath = `${parentUrl}/${path}`;
+
   const renderContactsItem = (list, label, type) => 
     list.map((curr, index) => (
       <div key={index} className={styles.contactsItem}>
-        { label }:
-        {' '}
+        { label }:&ensp;
         { type === 'tel' && <a href={`tel:${curr}`} className={styles.contact}>{curr}</a> }
         { type === 'email' && <a href={`mailto:${curr}`} className={styles.contact}>{ curr }</a> }
         { type === 'web' && <a href={curr} className={styles.contact}>{ curr }</a> }
@@ -27,16 +30,27 @@ const Office = ({ data: {city, addresses}, parentUrl }) => {
       </li>
     ));
 
-  return (
+  const renderCurrOffice = () =>
     <div className={styles.element}>
       <Link className={styles.caption} to={parentUrl}>
-        <ArrowIcon className={styles.arrowIcon}/>
+        <ArrowUpIcon className={styles.arrowIcon} />
         <div className={styles.city}>{ city }</div>
       </Link>
       <ul className={styles.list}>
         { renderAddresses() }
       </ul>
     </div>
+
+  const renderCurrOfficeLink = () =>
+    <Link to={{ pathname: currOfficePath, state: { prevLocation: 'offices' }}} className={styles.item}>
+      <ArrowDownIcon />
+      <div>{ city }</div>
+    </Link>
+
+  return (
+    location.pathname === currOfficePath
+      ? renderCurrOffice()
+      : renderCurrOfficeLink()
   );
 };
 
--- src/components/pages/Offices/Offices.js
+++ src/components/pages/Offices/Offices.js
@@ -1,52 +1,32 @@
 import React from 'react';
-import { Switch, Route, Link, useRouteMatch, matchPath, useLocation } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import cx from 'classnames';
 
+import Office from './Office/Office';
 import styles from './Offices.module.scss';
-import Office from './Office';
-import {ReactComponent as ArrowIcon} from './img/Down.svg';
 
 const Offices = ({ data: { meta, title, offices }, isRerouted }) => {
-  const { pathname } = useLocation();
-  const { path, url } = useRouteMatch(); 
+  const { url } = useRouteMatch();
 
-  const { params: { current } } = matchPath(pathname, { path: `${path}/:current` }) || {params: {}};
-  const currIndex = offices.map((o) => o.path).indexOf(current);
-  const officesBefore = offices.slice(0, currIndex);
-  const officesAfter = current ? offices.slice(currIndex+1) : [];
-
-  const officeLinks = (list) => list.map((item, index) => (
-    <Link to={`${url}/${item.path}`} className={styles.item}>
-      <ArrowIcon key={index} className={styles.arrowIcon}/>
-      <div key={index} className={styles.city}>{ item.city }</div>
-      <div key={index} className={styles.horizontalDividerWrap} style={{animationDelay: `${(index + 1) * 150}ms`}}>
-        <div key={index} className={styles.horizontalDivider} style={{animationDelay: `${(index + 1) * 200}ms`}}/>
+  const renderOffices = () =>
+    offices.map((item, index) => (
+      <div key={index} className={styles.officeWrap} style={{animationDelay: `${index * 100 + 1000}ms`}}>
+        <Office data={item} parentUrl={url} />
       </div>
-    </Link>
-  ));
-
-  const officeRoutes = (list) => list.map((item) => (
-    <Route path={`${path}/${item.path}`}>
-      <Office data={item} parentUrl={url} />
-    </Route>
-  ))
+    ));
 
   return (
     <>
       <div className={styles.container}>
       <Helmet>
-        <title>{meta.title}</title>
+        <title>{ meta.title }</title>
         <meta name="description" content={meta.description} />
         <meta name="keywords" content={meta.keywords} />
       </Helmet>
-        <div className={cx(styles.header, {[styles.headerNoDelay]: isRerouted})}>{title}</div>
+        <div className={cx(styles.header, {[styles.headerNoDelay]: isRerouted})}>{ title }</div>
         <div className={styles.content}>
-          {officeLinks(officesBefore)}
-          <Switch>
-            {officeRoutes(offices)}
-          </Switch>
-          {officeLinks(officesAfter)}
+          { renderOffices() }
         </div>
     </div>
     </>
