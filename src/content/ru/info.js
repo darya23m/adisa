a1e389f9bd2395daebe01a5111ac8a6c6009bdba
apolyakov88 <89034238+apolyakov88@users.noreply.github.com>
2021-08-19 10:34:06 +0300

    Update info.js

--- src/content/ru/info.js
+++ src/content/ru/info.js
@@ -8,27 +8,32 @@ export default {
   info: [ // resizable
     // ========== List1 ==========
     {
-      caption: "Сертификаты и разрешения",
+      caption: "Инструкции",
       list: [ // resizable
         {
-          type: "doc",
-          text: "ADI CD Modulating gas condensing boiler in stainless steel 70-375 кВт. Сертификат соответствия.",
-          link: "resources/example1.pdf"
-        },
-        {
-          type: "doc",
-          text: "ADI HT Very high efficiency 105-400 кВт. Сертификат соответствия.",
-          link: "resources/example1.pdf"
+          type: "link",
+          text: "Инструкция ADISA ADI LT",
+          link: "/resources/ADI-LT-Rus.pdf"
         },
         {
           type: "link",
-          text: "ADI LT Condensing modulating gas boiler in stainless steel 105-400 кВт. Сертификат соответствия.",
-          link: "resources/example1.pdf"
+          text: "Инструкция ADISA ADI CD",
+          link: "/resources/ADI-CD-Rus.pdf"
         },
         {
           type: "link",
-          text: "ADI HT Very high efficiency 475-750 кВт. Сертификат соответствия.",
-          link: "resources/example1.pdf"
+          text: "Инструкция ADISA ADI MEGA",
+          link: "/resources/ADI-MEGA-CD-Rus.pdf"
+        },
+        {
+          type: "link",
+          text: "Инструкция ADISA Condexa PRO",
+          link: "/resources/ADI-Condexa-PRO-Rus.pdf"
+        },
+        {
+          type: "link",
+          text: "Каскад ADISA Condexa PRO",
+          link: "/resources/ADI-Condexa-PRO-Cascade_Rus.pdf"
         }
       ],
     },
