3c53d9356ed36bae6ff1ceedb1cd4addba3f6559
darya23m <77686522+darya23m@users.noreply.github.com>
2021-09-04 20:01:51 +0300

    [AD-91] NoiseLvl img nama fix (#118)

--- src/components/pages/Home/NoiseLvl/NoiseLvl.js
+++ src/components/pages/Home/NoiseLvl/NoiseLvl.js
@@ -38,15 +38,15 @@ const NoiseLvl = ({ data: {
           <div className={styles.features}>
             <div className={styles.redLine} />
             <div className={styles.feature}>
-              <div className={styles.nameWrap}><div className={styles.name1}>{ featureName1 }</div></div>
+              <div className={styles.nameWrap}><div className={styles.name}>{ featureName1 }</div></div>
               <img src={feature1} alt={featureAlt1} className={styles.imageFeature} />
             </div>
             <div className={styles.feature}>
-            <div className={styles.nameWrap}><div className={styles.name2}>{ featureName2 }</div></div>
+            <div className={styles.nameWrap}><div className={styles.name}>{ featureName2 }</div></div>
               <img src={feature2} alt={featureAlt2} className={styles.imageFeature} />
             </div>
             <div className={styles.feature}>
-            <div className={styles.nameWrap}><div className={styles.name3}>{ featureName3 }</div></div>
+            <div className={styles.nameWrap3}><div className={styles.name}>{ featureName3 }</div></div>
               <img src={feature3} alt={featureAlt3} className={styles.imageFeature} />
             </div>
           </div>
