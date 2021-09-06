f2676f1372e4704ab02702f9a5966604d9c89cd2
Евгений Климентьев <jmnemo@MacBook-Pro-Evgenij.local>
2021-09-06 03:30:35 +0300

    noiseLvl chart animation / info and contacts bg moving effect

--- src/components/pages/Contacts/Contacts.js
+++ src/components/pages/Contacts/Contacts.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import cx from 'classnames';
 
@@ -7,7 +7,29 @@ import Offices from 'components/pages/Contacts/Offices/Offices';
 import ContactsForm from './ContactsForm/ContactsForm';
 
 const Contacts = ({ data, isRerouted, isAppFaded }) => {
+  const headerBgRef = useRef(null);
   const contactsHeaderRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = ({ x, y, view }) => {
+      window.requestAnimationFrame(() => {
+        const midX = view.innerWidth / 2;
+        const midY = 150;
+        const offsetX = (midX - x) * -0.001;
+        const offsetY = (midY - y) * -0.02;
+
+        headerBgRef.current.style.transform = (
+          `translate3d(${offsetX > 4 ? 4 : offsetX < -4 ? -4 : offsetX}%, ${offsetY > 8 ? 8 : offsetY < -8 ? -8 : offsetY}%, 0)`
+        );
+      });
+    }
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    }
+  }, []);
 
   return (
     <>
@@ -20,6 +42,7 @@ const Contacts = ({ data, isRerouted, isAppFaded }) => {
         <div ref={contactsHeaderRef}
              className={cx(styles.header, {[styles.headerNoDelay]: isRerouted, [styles.headerHidden]: isAppFaded})}
         >
+          <div className={styles.headerBg} ref={headerBgRef} />
           { data.header }
         </div>
         <div className={cx(styles.content, {[styles.contentHidden]: isAppFaded})}>
--- src/components/pages/Info/Info.js
+++ src/components/pages/Info/Info.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import { Helmet } from 'react-helmet';
 import cx from 'classnames';
 
@@ -6,7 +6,29 @@ import InfoList from './InfoList';
 import styles from './Info.module.scss';
 
 const Info = ({ data: { meta }, data, isRerouted, isAppFaded }) => {
+  const headerBgRef = useRef(null);
   const renderLists = lists => lists.map((list, index) => (<InfoList key={index} data={list} listIndex={index} />));
+
+  useEffect(() => {
+    const handleMouseMove = ({ x, y, view }) => {
+      window.requestAnimationFrame(() => {
+        const midX = view.innerWidth / 2;
+        const midY = 150;
+        const offsetX = (midX - x) * -0.001;
+        const offsetY = (midY - y) * -0.02;
+
+        headerBgRef.current.style.transform = (
+          `translate3d(${offsetX > 4 ? 4 : offsetX < -4 ? -4 : offsetX}%, ${offsetY > 8 ? 8 : offsetY < -8 ? -8 : offsetY}%, 0)`
+        );
+      });
+    }
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    }
+  }, []);
 
   return (
     <>
@@ -17,6 +39,7 @@ const Info = ({ data: { meta }, data, isRerouted, isAppFaded }) => {
       </Helmet>
       <div className={styles.container}>
         <div className={cx(styles.header, {[styles.headerNoDelay]: isRerouted, [styles.headerHidden]: isAppFaded})}>
+          <div className={styles.headerBg} ref={headerBgRef} />
           { data.title }
         </div>
         <div className={cx(styles.content, {[styles.contentHidden]: isAppFaded})}>
